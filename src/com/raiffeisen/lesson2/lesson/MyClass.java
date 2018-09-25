7841917b6df6c6bba22d5ff923e37ab8eafc6a09
Malov Nikolay <Nikolay.MALOV@raiffeisen.ru>
2018-09-25 16:08:11 +0300

    Лекция 2

---
+++ src/com/raiffeisen/lesson2/lesson/MyClass.java
@@ -0,0 +1,11 @@
+package com.raiffeisen.lesson2.lesson;
+
+public class MyClass implements Relatable {
+
+
+
+    @Override
+    public int isLArgerThan(Relatable other) {
+        return 0;
+    }
+}
---
+++ src/com/raiffeisen/lesson2/lesson/Relatable.java
@@ -0,0 +1,7 @@
+package com.raiffeisen.lesson2.lesson;
+
+public interface Relatable {
+
+    int isLArgerThan(Relatable other);
+
+}
