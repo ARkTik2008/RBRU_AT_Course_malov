026598d431e4ab3df2afa9b4327d9ce74c12cb2b
Malov Nikolay <Nikolay.Malov@raiffeisen.ru>
2018-10-17 20:08:17 +0300

    Lesson 10 17

--- src/com/raiffeisen/lesson_10_17_XML/xmlMain.java
+++ src/com/raiffeisen/lesson_10_17_XML/xmlMain.java
@@ -9,7 +9,12 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -19,13 +24,13 @@ import java.nio.file.Files;
 
 public class xmlMain {
 
-    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, TransformerException {
         Map<String, String> inData = parseFile();
 
         Document doc = readXml();
 
         inData.entrySet().forEach(e -> getNode(e.getKey(), doc).setTextContent(e.getValue()));
-        System.out.println(doc.getTextContent());
+        saveXML(doc);
     }
 
     private static Map<String, String> parseFile() throws IOException {
@@ -61,5 +66,13 @@ public class xmlMain {
         doc.getDocumentElement().normalize();
         return doc;
     }
+
+    public static void saveXML(Document doc) throws TransformerException, FileNotFoundException {
+        Transformer tr = TransformerFactory.newInstance().newTransformer();
+
+        tr.setOutputProperty(OutputKeys.METHOD,"xml");
+        tr.transform(new DOMSource(doc), new StreamResult(new FileOutputStream("C:/tmp/1.xml")));
+    }
+
 
 }
