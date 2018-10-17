991505436b08a0950bf745ccc36d24185511077e
Malov Nikolay <Nikolay.Malov@raiffeisen.ru>
2018-10-17 19:29:26 +0300

    Lesson 10 17

--- src/com/raiffeisen/lesson_10_17_XML/xmlMain.java
+++ src/com/raiffeisen/lesson_10_17_XML/xmlMain.java
@@ -1,6 +1,9 @@
 package com.raiffeisen.lesson_10_17_XML;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -16,14 +19,18 @@ import java.nio.file.Files;
 
 public class xmlMain {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         Map<String, String> inData = parseFile();
+
+        Document doc = readXml();
+
+        inData.entrySet().forEach(e->getNode(e.getKey(),doc).setTextContent(e.getValue()));
     }
 
     private static Map<String, String> parseFile() throws IOException {
 
         HashMap<String, String> data = new HashMap<>();
-        Files.readAllLines(Paths.get("C://Work/RBRU_AT_Course_malov/src/com/raiffeisen/1.xml")).forEach(
+        Files.readAllLines(Paths.get("C:/tmp/1.txt")).forEach(
                 l -> {
                     String[] tmp = l.split(":");
                     data.put(tmp[0].trim(), tmp[1].trim());
@@ -31,9 +38,20 @@ public class xmlMain {
         );
         return data;
     }
+    public static Element getNode(String name, Document document){
+        NodeList nList = document.getElementsByTagName("root");
+        for (int temp = 0; temp < nList.getLength(); temp++){
+            Node nNode = nList.item(temp);
+            if ((nNode.getNodeType() == Node.ELEMENT_NODE) &&(nNode.getNodeName().equals(name))){
+                return (Element) nNode;
+            }
+        }
+    return null;
+    }
+
 
     public static Document readXml() throws ParserConfigurationException, IOException, SAXException {
-        File fXmlFile = new File("C://Work/RBRU_AT_Course_malov/src/com/raiffeisen/1.xml");
+        File fXmlFile = new File("C:/tmp/1.xml");
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(fXmlFile);
