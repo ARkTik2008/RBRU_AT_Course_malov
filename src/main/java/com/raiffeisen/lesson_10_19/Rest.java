c91ceb40eff80034888dc2101fb62a68dbb485e0
Malov Nikolay <Nikolay.Malov@raiffeisen.ru>
2018-10-19 20:29:44 +0300

    Lesson 10 19

--- src/main/java/com/raiffeisen/lesson_10_19/Rest.java
+++ src/main/java/com/raiffeisen/lesson_10_19/Rest.java
@@ -3,12 +3,14 @@ package com.raiffeisen.lesson_10_19;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 
 
 public class Rest {
 
     public static void main(String[] args) {
        Response rs = get("https://www.cbr.ru/scripts/XML_daily.asp");
+        rs.getBody().xmlPath().get("\\valute[@IID = 'R01010']");
+        given().body("").when().get("").then();
     }
-
 }
